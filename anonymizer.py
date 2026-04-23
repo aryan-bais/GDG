c3c2313c52656647158f8026dec252490ff9653a
aryan-peg <aryanbais14@gmail.com>
2026-04-24 01:29:01 +0530

    Add files via upload

---
+++ anonymizer.py
@@ -0,0 +1,7 @@
+import re
+
+def anonymize(text):
+    text = re.sub(r'\b[A-Z][a-z]+ [A-Z][a-z]+\b', '[NAME]', text)
+    text = re.sub(r'\b\d{10}\b', '[PHONE]', text)
+    text = re.sub(r'\S+@\S+', '[EMAIL]', text)
+    return text
